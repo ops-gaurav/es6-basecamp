3d6d37878301dc67f8b548b8fe6abe03c7d13155
gaurav <sharma02gaurav@gmail.com>
2017-04-28 06:18:06 -0700

    loader script loaded

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,21 @@
 
 $('.app-body').css ('margin-top', '100px');
+$('.loader-container-screen').css ('display', 'block');
+var alignLoader = function () {
+	var xpos = ($(window).width() - $('.loader').width())/2;
+	var ypos = ($(window).height() - $('.loader').height())/2 - 50;
+
+	$('.loader').css ('margin-left', xpos+'px');
+	$('.loader').css ('margin-top', ypos+'px');
+}
+
+alignLoader();
+
+$(document).ready (function (){
+	// alert ('done loading');
+	$('.loader-container-screen').animate ({
+		'opacity': 0
+	}, 500, function () {
+		$('.loader-container-screen').css ('display', 'none');
+	});
+});
